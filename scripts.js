e244d3ef5038f0ec1d6ccb2bf6f0606da81d572d
Liam <liambsullivan@gmail.com>
2023-11-10 00:40:19 -0500

    more gui tweaks

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //* Globals
 const calcBody = document.querySelector('.calc-body');
-
+const calcDisplay = document.querySelector('.calc-display');
 
 //* Basic Arithmetic
 const add = (a, b) => a + b;
@@ -8,7 +8,7 @@ const sub = (a, b) => a - b;
 const mult = (a, b) => a * b;
 const div = (a, b) => a / b;
 
-//* Draw Display
+//* Draws Button Interface
 function drawDisplay () {
     for (let i = 1; i <= 9; i++) {
         const numBtn = document.createElement('button');
@@ -16,6 +16,20 @@ function drawDisplay () {
         numBtn.textContent = i + '';
         calcBody.append(numBtn);
     }
+    const ops = ['+', '-', '*', '/', '='];
+    for (let i = 0; i < 5; i++) {
+        const opBtn = document.createElement('button');
+        opBtn.className = 'opBtn';
+        opBtn.textContent = ops[i];
+        calcBody.append(opBtn);
+    }
+}
+
+//* Draws numbers to the screen when an update is requested
+//! Missing Implementation
+function drawNums () {
+    calcDisplay.textContent = 0;
 }
 
 drawDisplay();
+drawNums();
